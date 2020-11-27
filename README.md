89b0c3a901d7aff13166dbbd894f442d1d63261a
ZhaoChongyan <zhaochongyan2005@outlook.com>
2020-11-27 10:35:28 +0800

    修改：     README.md 	新文件：   cfviz/.gitignore 	新文件：   cfviz/README.md 	新文件：   cfviz/about.html 	新文件：   cfviz/compare.html 	新文件：   cfviz/images/android-desktop.png 	新文件：   cfviz/images/favicon.png 	新文件：   cfviz/images/icon.png 	新文件：   cfviz/images/ios-desktop.png 	新文件：   cfviz/images/og.jpg 	新文件：   cfviz/index.html 	新文件：   cfviz/js/calculate.js 	新文件：   cfviz/js/compare.js 	新文件：   cfviz/js/compare_helper.js 	新文件：   cfviz/js/single.js 	新文件：   cfviz/js/vir.js 	新文件：   cfviz/manifest.json 	新文件：   cfviz/styles/style.css 	新文件：   cfviz/sw.js 	新文件：   cfviz/virtual-rating-change.html 	新文件：   lib/install.sh

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Welcome to GitHub Pages
 
 [带我去打球](https://zhaochongyan.github.io/cxkball/)
+[cfviz](https://zhaochongyan.github.io/cfviz/)
---
+++ cfviz/sw.js
@@ -0,0 +1,22 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+);
+
+workbox.precaching.precacheAndRoute([
+  { url: '/index.html', revision: '1118' },
+  { url: '/about.html', revision: '1114' },
+  { url: '/compare.html', revision: '1114' },
+  { url: '/virtual-rating-change.html', revision: '1116' },
+  { url: '/js/compare_helper.js', revision: '1113' },
+  { url: '/js/compare.js', revision: '1111' },
+  { url: '/js/calculate.js', revision: '1112' },
+  { url: '/js/single.js', revision: '1112' },
+  { url: '/js/vir.js', revision: '1113' },
+]);
+
+workbox.routing.registerRoute(
+  /^((?!codeforces)(?!facebook)(?!analytics)(?!ads)(?!google).)*$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'local',
+  })
+);
